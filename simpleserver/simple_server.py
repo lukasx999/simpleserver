e7e197c6d0b165f59704e427a00e489a4f486d23
Robert Ulmer <robert@familieulmer.info>
2024-12-02 11:10:08 +0100

    allow socket server to reuse address

--- simpleserver/simple_server.py
+++ simpleserver/simple_server.py
@@ -8,20 +8,38 @@ logging.basicConfig(level=logging.DEBUG)
 class MyTCPHandler(socketserver.BaseRequestHandler):
 
     def handle(self):
+        self.request.sendall(
+            b"Hi there: " + self.client_address[0].encode("utf-8") + b"\n"
+        )
+        self.request.sendall(b"Type 'quit' to quit.\n")
         while True:
             self.data = self.request.recv(1024).strip()
             logger.debug("Received from %s: %s", self.client_address[0], self.data)
-            self.request.sendall(self.data.upper() + b"\n")
+            self.request.sendall(
+                self.data + b" is in upper case: " + self.data.upper() + b"\n"
+            )
             if self.data.upper() == b"QUIT":
                 break
         logger.debug("Client %s left", self.client_address[0])
 
 
-def main():
+class MyTCPServer(socketserver.TCPServer):
+
     HOST, PORT = "0.0.0.0", 9999
 
-    with socketserver.TCPServer((HOST, PORT), MyTCPHandler) as server:
-        logger.debug(f"Listening to {HOST}:{PORT}")
+    def __init__(self):
+        self.allow_reuse_address = True
+        super().__init__((MyTCPServer.HOST, MyTCPServer.PORT), MyTCPHandler)
+
+    def serve_forever(self, poll_interval=0.5):
+        logging.debug(f"Listening to: {MyTCPServer.HOST}:{MyTCPServer.PORT}")
+        super().serve_forever(poll_interval)
+
+
+def main():
+
+    with MyTCPServer() as server:
+
         server.serve_forever()
 
 
